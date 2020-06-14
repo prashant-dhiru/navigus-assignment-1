24dae7d0a6cc5387463b6e0741dbb37b58cd7c2b
prashant <prashant.dhirendra@gmail.com>
2020-06-14 18:31:13 +0530

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname+"/frontend/dist/"));
 app.get(/.*/,function(req,res){
-    res.sendFile(__dirname+"frontend/dist/index.html");
+    res.sendFile(__dirname+"/frontend/dist/index.html");
 });
 
 app.listen(port);
